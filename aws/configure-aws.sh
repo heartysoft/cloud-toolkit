1ab3c9864e192995dd4b8f015d169223f1d7382b
ashic <ashic@live.com>
2017-07-07 13:40:21 +0100

    unsets aws keys after at the end of configuring if ROLE_ARN is provided

--- aws/configure-aws.sh
+++ aws/configure-aws.sh
@@ -10,23 +10,38 @@ profile_name=${AWS_PROFILE_NAME:-aws-user}
 
 echo "[default]" >> ~/.aws/config
 echo "region=$REGION" >> ~/.aws/config
-echo "role_arn=$ROLE_ARN" >> ~/.aws/config
-echo "source_profile=$profile_name" >> ~/.aws/config
 
-echo "" >> ~/.aws/config
+if [ -z $ROLE_ARN ]; then
+  echo "role_arn=$ROLE_ARN" >> ~/.aws/config
+fi
 
-echo "[profile $profile_name]" >> ~/.aws/config
-echo "region=$REGION" >> ~/.aws/config
-echo "role_arn=$ROLE_ARN" >> ~/.aws/config
 echo "source_profile=$profile_name" >> ~/.aws/config
 
 if [ "x$EXTERNAL_ID" != "x" ]; then
   echo "external_id=$EXTERNAL_ID" >> ~/.aws/config
 fi
 
-# needs AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY to be set.
+echo "" >> ~/.aws/config
+
+echo "[profile $profile_name]" >> ~/.aws/config
+echo "region=$REGION" >> ~/.aws/config
+
+if [ -z $ROLE_ARN ]; then
+  echo "role_arn=$ROLE_ARN" >> ~/.aws/config
+fi
+
+echo "source_profile=$profile_name" >> ~/.aws/config
+
+if [ "x$EXTERNAL_ID" != "x" ]; then
+  echo "external_id=$EXTERNAL_ID" >> ~/.aws/config
+fi
 
 echo "[$profile_name]" >> ~/.aws/credentials
 echo "aws_access_key_id=$AWS_ACCESS_KEY_ID" >> ~/.aws/credentials
 echo "aws_secret_access_key=$AWS_SECRET_ACCESS_KEY" >> ~/.aws/credentials
+
+if [ -z $ROLE_ARN ]; then
+  unset AWS_ACCESS_KEY_ID
+  unset AWS_SECRET_ACCESS_KEY
+fi
 
